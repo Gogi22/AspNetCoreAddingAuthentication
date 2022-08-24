c848abf085b16908510df1c0e7af2cc1c8e22942
Gogi <gogi.mamutchadze@lb.ge>
2022-08-24 23:10:30 +0400

    passed register tests

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
@@ -16,7 +18,33 @@ namespace WishList.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
         
-        
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = _userManager.CreateAsync(new ApplicationUser { Email = model.Email, UserName = model.Email },
+                model.Password).GetAwaiter().GetResult();
+
+            if (result.Succeeded) return new RedirectToActionResult( "Index", "Home", new{});
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("Password", error.Description);
+            }
+
+            return View(model);
+        }
     }
 }
